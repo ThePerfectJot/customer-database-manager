db2fa776c1a29eedd6e14a9f668054e158fffd51
ThePerfectJot <78319306+ThePerfectJot@users.noreply.github.com>
2021-01-31 14:26:27 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const port = 7777;
+
+http.createServer((request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+        // BEGINING OF NEW STUFF 
+
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // At this point, we have the headers, method, url and body, and can now
+        // do whatever we need to in order to respond to this request.
+
+        // BEGINING OF NEW STUFF
+       
+        response.on('error', (err) => {
+            console.error(err);
+        });
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.writeHead(200, {'Content-Type': 'application/json'})
+
+        const responseBody = { headers, method, url, body };
+        response.write(JSON.stringify(responseBody));
+
+        response.write('<html><body><h1>=== Area: Root ===</h1></body></html>')
+
+        response.end();
+        // END OF NEW STUFF
+
+    });
+
+}).listen(port); // Activates this server, listening on port 7777.
+console.log('Server listening on port 7777')
